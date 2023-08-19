bd182616d79750de42eefe09c2113bd903b36e39
Francis Ong <francis.ong25@gmail.com>
2023-08-19 14:32:40 +0800

    feat: add empty state component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 
 import React from 'react';
-import { signOut } from 'next-auth/react';
+// import { signOut } from 'next-auth/react';
+
+import EmptyState from '../components/EmptyState';
 
 const Users = () => {
-  return <button onClick={() => signOut()}>Logout</button>;
+  return (
+    <main className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </main>
+  );
 };
 
 export default Users;
