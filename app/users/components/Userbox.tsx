b370875324d4f5b82410d0af15d4a4b1d9d04e26
Francis Ong <francis.ong25@gmail.com>
2023-08-20 19:35:47 +0800

    feat: add users convo list components

---
+++ app/users/components/Userbox.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import axios from 'axios';
+import { User } from '@prisma/client';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import React, { useCallback } from 'react';
+
+import Avatar from '@/app/components/Sidebar/Avatar';
+
+interface UserboxProps {
+  user: User;
+}
+
+const Userbox: React.FC<UserboxProps> = ({ user }) => {
+  const router = useRouter();
+
+  const handleClick = useCallback(async () => {
+    try {
+      toast.loading('Wait for a moment...');
+
+      const response = await axios.post('/api/conversations', {
+        userId: user.id,
+      });
+
+      router.push(`/conversations/${response.data.id}`);
+    } catch (error: unknown) {
+      toast.error('Something went wrong');
+    } finally {
+      toast.dismiss();
+    }
+  }, [user.id, router]);
+
+  return (
+    <div
+      onClick={handleClick}
+      className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+    >
+      <Avatar user={user} />
+      <div className="min-2-0 flex-1">
+        <div className="focus:outline-none">
+          <div className="flex justify-between items-center mb-1">
+            <p className="text-sm font-medium text-gray-900">{user.name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Userbox;
--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+
+import getUsers from '../actions/getUsers';
 import SideBar from '../components/Sidebar';
+import UsersList from './components/UsersList';
 
 const UsersLayout = async ({ children }: { children: React.ReactNode }) => {
+  const users = await getUsers();
+
   return (
     <SideBar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UsersList users={users} />
+        {children}
+      </div>
     </SideBar>
   );
 };
