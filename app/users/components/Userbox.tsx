185a59690a0a7508d5a907252007dd0ef59033c0
Francis Ong <francis.ong25@gmail.com>
2023-08-29 22:39:51 +0800

    feat: implement loading to users and convo pages

--- app/users/components/Userbox.tsx
+++ app/users/components/Userbox.tsx
@@ -4,9 +4,10 @@ import axios from 'axios';
 import { User } from '@prisma/client';
 import { toast } from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import Avatar from '@/app/components/Sidebar/Avatar';
+import LoadingModal from '@/app/components/LoadingModal';
 
 interface UserboxProps {
   user: User;
@@ -14,11 +15,11 @@ interface UserboxProps {
 
 const Userbox: React.FC<UserboxProps> = ({ user }) => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = useCallback(async () => {
-    const toastLoading = toast.loading('Wait for a moment...');
-
     try {
+      setIsLoading(true);
       const response = await axios.post('/api/conversations', {
         userId: user.id,
       });
@@ -27,24 +28,27 @@ const Userbox: React.FC<UserboxProps> = ({ user }) => {
     } catch (error: unknown) {
       toast.error('Something went wrong');
     } finally {
-      toast.dismiss(toastLoading);
+      setIsLoading(false);
     }
   }, [user.id, router]);
 
   return (
-    <div
-      onClick={handleClick}
-      className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
-    >
-      <Avatar user={user} />
-      <div className="min-2-0 flex-1">
-        <div className="focus:outline-none">
-          <div className="flex justify-between items-center mb-1">
-            <p className="text-sm font-medium text-gray-900">{user.name}</p>
+    <>
+      {isLoading && <LoadingModal />}
+      <div
+        onClick={handleClick}
+        className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+      >
+        <Avatar user={user} />
+        <div className="min-2-0 flex-1">
+          <div className="focus:outline-none">
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">{user.name}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
