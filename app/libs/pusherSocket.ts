6cadc6abd3183ca087969c0258f536829a0f99f6
Francis Ong <francis.ong25@gmail.com>
2023-09-03 21:44:53 +0800

    feat: setup for real time channel for active users

--- app/libs/pusherSocket.ts
+++ app/libs/pusherSocket.ts
@@ -11,5 +11,11 @@ export const pusherServer = new PusherServer({
 
 export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  { cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER! },
+  {
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+    channelAuthorization: {
+      endpoint: '/api/pusher/auth',
+      transport: 'ajax',
+    },
+  },
 );
